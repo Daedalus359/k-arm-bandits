e2a571bab55f9700f37f4426c44ed103aad9af1c
= <=>
2020-02-24 13:55:54 -0800

    generating values from the normal distribution

--- src/KAB.hs
+++ src/KAB.hs
@@ -1,3 +1,12 @@
 module KAB where
 
+import Statistics.Distribution
+import Statistics.Distribution.Normal
+import System.Random.MWC
+
 testComp = "."
+
+normalDist = normalDistr 0 1
+
+sampleNormal :: IO Double
+sampleNormal = withSystemRandom (genContVar normalDist :: (GenIO -> IO Double))
